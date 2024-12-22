8fa28d2535798876e580d5d02db5255cfdd55563
Eslam Eltamalawy <eslameltamalawy124@gmail.com>
2024-12-22 21:55:31 +0200

    Add files via upload

---
+++ Migrations/20230809093634_addusermigration11.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC1.Migrations
+{
+    public partial class addusermigration11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Emile = table.Column<string>(nullable: true),
+                    Passwored = table.Column<string>(nullable: true),
+                    Rol = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ Models/Books.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC1.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name Is Required")]
+        [MinLength(5, ErrorMessage = "Name Must Be 5 Char Min")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Price Is Required")]
+        [Range(1, int.MaxValue)]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Category name Is Required")]
+        [Range(1, int.MaxValue)]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [Required(ErrorMessage = "Author name Is Required")]
+        [Range(1, int.MaxValue)]
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+    }
+}
