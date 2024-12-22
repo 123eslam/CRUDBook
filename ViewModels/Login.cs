2fdb94d8f411f68ed4982cd7cc378feeb678bb25
Eslam Eltamalawy <eslameltamalawy124@gmail.com>
2024-12-22 21:49:13 +0200

    Add files via upload

---
+++ ViewModels/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC1.ViewModels
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Passwored { get; set; }
+
+    }
+}
